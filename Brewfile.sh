ed224c4b5e314172f57baefac0b3ec73e86de106
Alper Cugun <alper@users.noreply.github.com>
2020-11-04 21:49:49 +0100

    Create Brewfile.sh

---
+++ Brewfile.sh
@@ -0,0 +1,40 @@
+brew tap clementtsang/bottom
+brew tap heroku/brew
+brew tap osx-cross/avr
+brew tap px4/px4
+
+brew install clementtsang/bottom/bottom
+brew install clang-format
+brew install cloc
+brew install clojure
+brew install curl
+brew install exa
+brew install fd
+brew install ffmpeg
+brew install fish
+brew install gcc
+brew install git
+brew install go
+brew install heroku/brew/heroku
+brew install highlight
+brew install imagemagick
+brew install jq
+brew install leiningen
+brew install lua
+brew install node
+brew install openssh
+brew install pipenv
+brew install postgresql
+brew install pyenv
+brew install python
+brew install ripgrep
+brew install rust
+brew install the_silver_searcher
+brew install wget
+brew install youtube-dl
+
+brew cask install font-iosevka
+brew cask install font-iosevka-slab
+brew cask install java
+brew cask install menumeters
+brew cask install qlmarkdown
