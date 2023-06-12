7fc63be41ec3217ab8bd0ffd141da82866757a03
Alper Cugun <github@alper.nl>
2023-06-12 10:20:51 +0200

    Prepare to remove and nixify most of the things in here

--- Brewfile.sh
+++ Brewfile.sh
@@ -1,43 +1,47 @@
-# TODO
-# Remove all languages and move that to rtx
-# Check Postgres install
-
-# brew tap heroku/brew
 # brew tap osx-cross/avr
 # brew tap px4/px4
 
-brew install bat
-brew install bottom
-brew install clang-format
-brew install cloc
-brew install cmake
-brew install curl
-brew install deno
-brew install dog
-brew install duf
-brew install emscripten
-brew install exa
-brew install fd
-brew install ffmpeg
 brew install fish
 brew install fisher
-brew install fzf
-brew install gh
-brew install git
-brew install helix
-brew install ical-buddy
-brew install imagemagick
-brew install jless
-brew install jq
-brew install make
-brew install ncdu
 brew install openssh
-brew install ripgrep
-brew install sd
 brew install starship
-brew install the_silver_searcher
-brew install wget
-brew install yt-dlp
+
+# Check whether we still need this
+# brew install ncdu
+# brew install jless
+
+# Remove entirely
+# brew install clang-format
+# brew install cmake
+# brew install deno
+# brew install --cask menumeters
+# brew install --cask qlmarkdown
+# brew install emscripten
+# brew install --cask java
+# brew install make
+
+# Move to nix-profile
+# brew install bat
+# brew install bottom
+# brew install cloc
+# brew install curl
+# brew install dog
+# brew install duf
+# brew install exa
+# brew install fd
+# brew install ffmpeg
+# brew install fzf
+# brew install gh
+# brew install git
+# brew install ical-buddy
+# brew install imagemagick
+# brew install helix
+# brew install ripgrep
+# brew install jq
+# brew install sd
+# brew install the_silver_searcher
+# brew install wget
+# brew install yt-dlp
 
 brew tap homebrew/cask-fonts
 brew install --cask font-jetbrains-mono-nerd-font
@@ -45,6 +49,3 @@ brew install --cask font-iosevka
 brew install --cask font-iosevka-slab
 brew install --cask font-iosevka-nerd-font
 
-brew install --cask java
-brew install --cask menumeters
-brew install --cask qlmarkdown
