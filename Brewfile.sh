f22855116f32f7de601eca909eda09922dcd15f0
Alper Cugun <github@alper.nl>
2023-09-25 16:30:58 +0200

    Update Brewfile.sh

--- Brewfile.sh
+++ Brewfile.sh
@@ -1,51 +1,37 @@
 # brew tap osx-cross/avr
 # brew tap px4/px4
 
+# Core to make the shell work
 brew install fish
 brew install fisher
 brew install openssh
 brew install starship
+brew install git
 
-# Check whether we still need this
-# brew install ncdu
-# brew install jless
-
-# Remove entirely
-# brew install clang-format
-# brew install cmake
-# brew install deno
-# brew install --cask menumeters
-# brew install --cask qlmarkdown
-# brew install emscripten
-# brew install --cask java
-# brew install make
-
-# Move to nix-profile
-# brew install bat
-# brew install bottom
-# brew install cloc
-# brew install curl
-# brew install dog
-# brew install duf
-# brew install exa
-# brew install fd
-# brew install ffmpeg
-# brew install fzf
-# brew install gh
-# brew install git
-# brew install ical-buddy
-# brew install imagemagick
-# brew install helix
-# brew install ripgrep
-# brew install jq
-# brew install sd
-# brew install the_silver_searcher
-# brew install wget
-# brew install yt-dlp
+brew install bat
+brew install bottom
+brew install cloc
+brew install curl
+brew install dog
+brew install duf
+brew install exa
+brew install fd
+brew install ffmpeg
+brew install fzf
+brew install gh
+brew install ical-buddy
+brew install imagemagick
+brew install helix
+brew install ripgrep
+brew install jq
+brew install sd
+brew install the_silver_searcher
+brew install wget
+brew install yt-dlp
 
 brew tap homebrew/cask-fonts
 brew install --cask font-jetbrains-mono-nerd-font
-brew install --cask font-iosevka
-brew install --cask font-iosevka-slab
-brew install --cask font-iosevka-nerd-font
+# brew install --cask font-iosevka
+# brew install --cask font-iosevka-slab
+# brew install --cask font-iosevka-nerd-font
 
