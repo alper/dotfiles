18f8b34e0aeb45ad47e2f63ae9eae82fd216c2ad
Alper Cugun <github@alper.nl>
2024-07-18 14:50:05 +0200

    Update Brewfile.sh

--- Brewfile.sh
+++ Brewfile.sh
@@ -10,8 +10,11 @@ brew install git
 
 brew install bat
 brew install bottom
+brew install brimdata/tap/zq
+brew install btop
 brew install cloc
 brew install curl
+brew install difftastic
 brew install dog
 brew install duf
 brew install eza
@@ -20,15 +23,25 @@ brew install ffmpeg
 brew install fzf
 brew install gh
 brew install git-absorb
+brew install git-town
+brew install helix
 brew install ical-buddy
 brew install imagemagick
-brew install helix
+brew install jj
 brew install jq
 brew install lsd
+brew install moar
+brew install oven-sh/bun/bun
+brew install pipenv
+brew install pyenv
 brew install ranger
+brew install remotemobprogramming/brew/mob
 brew install ripgrep
+brew install ruff
+brew install rye
 brew install sd
 brew install the_silver_searcher
+brew install uv
 brew install wget
 brew install yt-dlp
 
