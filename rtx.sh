ed648b206db9947aadfa5e70f263bba5f29b8dd3
Alper Cugun <github@alper.nl>
2024-09-12 11:19:27 +0200

    Delete rtx.sh

--- rtx.sh
+++
@@ -1,8 +0,0 @@
-rtx global python@3.9.16
-rtx global rust@nightly
-rtx global nodejs@19.6.0
-rtx global racket@8.8
-rtx global haskell@9.4.4
-rtx global zig@0.10.1
-rtx global clojure@1.11.1.1208
-rtx global java@oracle-19.0.2
